fc8e363705cc22be1ed5973a175db428fa9e0783
Tadas Sasnauskas <tadas@yoyo.lt>
2026-04-16 13:59:15 +0300

    Set up app to work behind Apache reverse proxy.

--- src/elpako_issue/forwarder/server.py
+++ src/elpako_issue/forwarder/server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 cors = CORS(
     app,
     resources={
